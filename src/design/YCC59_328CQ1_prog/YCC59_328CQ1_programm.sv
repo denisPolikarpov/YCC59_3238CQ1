1ef9fa73fb87fb596200ab11832cae32e94883b1
Denis Polikarpov <polikarpov.denv.2@gmail.com>
2026-03-09 17:55:51 +0300

    Connections in top module

--- src/design/YCC59_328CQ1_prog/YCC59_328CQ1_programm.sv
+++ src/design/YCC59_328CQ1_prog/YCC59_328CQ1_programm.sv
@@ -22,15 +22,14 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-import YCC59_328CQ1_pkg::*;
-
 module YCC59_328CQ1_programm
 #(
     parameter int unsigned MAIN_CLK_SIGNAL       = 120000000,
     parameter int unsigned UNLOCK_SEQ_START_ADDR = 'h00,
     parameter int unsigned PROG_DATA_START_ADDR  = 'h04,
     parameter int unsigned TR_EN_OTHER_ADDR      = 'h18,
-    parameter int unsigned SELFTEST_START_ADDR   = 'h19
+    parameter int unsigned SELFTEST_START_ADDR   = 'h19,
+    parameter int unsigned MEM_FINAL_ADDRESS     = 'h2C
 )
 (
     input  logic i_clk,
@@ -46,21 +45,37 @@ module YCC59_328CQ1_programm
     input  logic i_DOUT,
     // Function SPI
     output logic o_FEN,
-                 o_FIN
+                 o_FIN,
+    // Test outputs
+    output logic [4 : 0] o_main_fsm_state,
+           logic [3 : 0] o_boot_fsm_state
 );
     // -----------------------------------------------
     // Defenitions
+    // Ctrl sequance
+    localparam logic [11 : 0] BOOT_FUNC_SEQ = 12'h380;
+    localparam logic [11 : 0] LATCH_TRANNS  = 12'h11F;
+    // Internal constants
     localparam int unsigned MEM_WIDTH     = 8;
     localparam int unsigned MEM_DEPTH     = 'h29;
-    localparam int unsigned ADDR_WIDTH    = 16;
+    localparam int unsigned ADDR_WIDTH    = 8;
     localparam int unsigned SPI_SCLK_FREQ = 20000000;
     
-    wire reset_addres_counter,
-         enable_addres_counter,
-         BRAM_read_write;
-    wire [ADDR_WIDTH - 1 : 0] beging_address_to_counter;
-    start_addr_mux_te start_addr_mux;
-    // -----------------------------------------------
+    wire boot_start_func_trans,
+         reset_counter,
+         enable_counter,
+         func_spi_seq,
+         spi_data_trans_finished,
+         spi_func_trans_finished;
+    wor  start_data_trans,
+         start_func_trans;
+         
+    wire [1 : 0] start_finish_address_mux_ctrl;
+    
+    wire [ADDR_WIDTH - 1 : 0] start_address,
+                              final_address;
+    wire [$bits(LATCH_TRANNS) - 1 : 0] func_seq_to_trans;
+    // ----------------------------------------------------------------------------------------------
     // Interfaces
     SPI_intr SPI_DATA();
     
@@ -97,8 +112,8 @@ module YCC59_328CQ1_programm
 
     assign BRAM_intr_to_SPI.rst = '0;
     assign BRAM_intr_to_SPI.en  = '1;
-    assign BRAM_intr_to_SPI.we  = '{default : BRAM_read_write};
-    // -----------------------------------------------
+    //assign BRAM_intr_to_SPI.we  = (main_fsm_state == 5'b01000) ? '1 : '0;
+    // ----------------------------------------------------------------------------------------------
     // BRAM for options
     BRAM
     #(
@@ -112,103 +127,161 @@ module YCC59_328CQ1_programm
         .mem_bus_a ( BRAM_intr_to_mem ),
         .mem_bus_b ( BRAM_intr_to_SPI )
     );
-    // -----------------------------------------------
-    // Addres counter
+    // ----------------------------------------------------------------------------------------------
+    // Address counter
     counter
     #(
-        .COUNTER_WIDTH      (     MEM_DEPTH    ),
-        .START_VALUE_SOURCE (      "PORT"      ),  // "PARAMETER"  // "PORT"
-        .START_VALUE        (         0        ),
-        .FINAL_VALUE_SOURCE (    "PARAMETER"   ),  // "PARAMETER"  // "PORT"
-        .FINAL_VALUE        ( 2**MEM_DEPTH - 1 )
+        .COUNTER_WIDTH      ( ADDR_WIDTH ),
+        .START_VALUE_SOURCE (   "PORT"   ),  // "PARAMETER"  // "PORT"
+        .START_VALUE        (      0     ),
+        .FINAL_VALUE_SOURCE (   "PORT"   ),  // "PARAMETER"  // "PORT"
+        .FINAL_VALUE        (  2**16 - 1 )
     )
-    addres_counter
+    address_counter
     (
         .i_clk,
-        .i_reset               (    reset_addres_counter   ),
-        .i_enable              (   enable_addres_counter   ),
-        .i_start_value         ( beging_address_to_counter ),
-        .i_final_value         (             '0            ),
-        .o_value               (   BRAM_intr_to_SPI.addr   ),
-        .o_final_value_reached (                           )
+        .i_reset               (     reset_counter     ),
+        .i_enable              (     enable_counter    ),
+        .i_start_value         (     start_address     ),
+        .i_final_value         (     final_address     ),
+        .o_value               ( BRAM_intr_to_SPI.addr ),
+        .o_final_value_reached (                       )
     );
     
     general_mux
     #(
-        .INPUT_WIDTH    ( MEM_DEPTH ),
-        .NUM_OF_SIGNALS (     4     )
+        .INPUT_WIDTH    ( ADDR_WIDTH ),
+        .NUM_OF_SIGNALS (      4     ),
+        .SYNC_OR_ASYNC  (   "ASYNC"  )  // "SYNC"   // "ASYNC"
     )
-    mux_start_address
+    start_adress_mux
     (
-        .i_clk,
-        .i_ctrl    (                                     start_addr_mux                                    ),
+        .i_clk, 
+        .i_ctrl    (                             start_finish_address_mux_ctrl                             ),
         .i_signals ( '{SELFTEST_START_ADDR, TR_EN_OTHER_ADDR, PROG_DATA_START_ADDR, UNLOCK_SEQ_START_ADDR} ),
-        .o_signal  (                               beging_address_to_counter                               )
+        .o_signal  (                                     start_address                                     )
     );
     
-    // -----------------------------------------------
-    // Data SPI
-    SPI_master
+    general_mux
     #(
-        .INPUT_WIDTH    (        8        ),
-        .MAIN_CLK_FREQ  ( MAIN_CLK_SIGNAL ),
-        .SCLK_FREQ      (  SPI_SCLK_FREQ  ),
-        .SCLK_NOT_END   (      "YES"      ),     // "YES"    // "NO"
-        .TRANSMIT_ORDER (      "MSB"      )      // "MSB"    // "LSB"
+        .INPUT_WIDTH    ( ADDR_WIDTH ),
+        .NUM_OF_SIGNALS (      4     ),
+        .SYNC_OR_ASYNC  (   "ASYNC"  )  // "SYNC"   // "ASYNC"
     )
-    SPI_master_DATA
+    final_adress_mux
+    (
+        .i_clk, 
+        .i_ctrl    (                          start_finish_address_mux_ctrl                         ),
+        .i_signals ( '{MEM_FINAL_ADDRESS, TR_EN_OTHER_ADDR, TR_EN_OTHER_ADDR, PROG_DATA_START_ADDR} ),
+        .o_signal  (                                  final_address                                 )
+    );
+    // ----------------------------------------------------------------------------------------------
+    // SPIs
+    data_spi
+    #(
+        .MAIN_CLK_SIGNAL ( MAIN_CLK_SIGNAL ),
+        .SPI_SCLK_FREQ   (  SPI_SCLK_FREQ  )
+    )
+    data_spi_inst
     (
         .i_clk,
-        // SPI master interface 
-        .intr_SPI_master ( SPI_DATA ),
-        // Data to transfer
-        .i_data  ( BRAM_intr_to_SPI.dout ),
-        .i_start ( ),
-        // Recieved data
-        .o_recieved_data ( BRAM_intr_to_SPI.din ),
-        .o_data_valid    ( )
+        // SPI interface
+        .intr_SPI_master      (         SPI_DATA        ),
+        // Other signals
+        .i_start              (     start_data_trans    ),
+        .i_data_from_bram     (  BRAM_intr_to_SPI.dout  ),
+        .o_enable_address_cn  (      enable_counter     ),
+        .o_trans_finished     (                         ),
+        .o_all_trans_finished ( spi_data_trans_finished )
     );
-    // -----------------------------------------------
-    // SPI fo function line
+    
+    general_mux
+    #(
+        .INPUT_WIDTH    ( $bits(LATCH_TRANNS) ),
+        .NUM_OF_SIGNALS (          2          ),
+        .SYNC_OR_ASYNC  (       "ASYNC"       )  // "SYNC"   // "ASYNC"
+    )
+    func_seq_mux
+    (
+        .i_clk, 
+        .i_ctrl    (          func_spi_seq          ),
+        .i_signals ( '{LATCH_TRANNS, BOOT_FUNC_SEQ} ),
+        .o_signal  (        func_seq_to_trans       )
+    );
+    
     SPI_master
     #(
-        .INPUT_WIDTH    (        8        ),
-        .MAIN_CLK_FREQ  ( MAIN_CLK_SIGNAL ),
-        .SCLK_FREQ      (     20000000    ),
-        .SCLK_NOT_END   (      "YES"      ),     // "YES"    // "NO"
-        .TRANSMIT_ORDER (      "MSB"      )      // "MSB"    // "LSB"
+        .INPUT_WIDTH    ( $bits(LATCH_TRANNS) ),
+        .MAIN_CLK_FREQ  (   MAIN_CLK_SIGNAL   ),
+        .SCLK_FREQ      (    SPI_SCLK_FREQ    ),
+        .SCLK_NOT_END   (        "YES"        ),     // "YES"    // "NO"
+        .TRANSMIT_ORDER (        "MSB"        )      // "MSB"    // "LSB"
     )
-    SPI_master_FUNCTION
+    SPI_function
     (
         .i_clk,
         // SPI master interface 
         .intr_SPI_master ( SPI_FUNC ),
         // Data to transfer
-        .i_data  ( ),
-        .i_start ( ),
+        .i_data  ( func_seq_to_trans ),
+        .i_start (  start_func_trans ),
         // Recieved data
-        .o_recieved_data ( ),
-        .o_data_valid    ( )
+        .o_recieved_data   (                         ),
+        .o_data_valid      ( spi_func_trans_finished ),
+        .o_transfer_active ( )
     );
-    // -----------------------------------------------
-    // Main FSM
+    // ----------------------------------------------------------------------------------------------
+//    // Delay line 
+//    // First clk cycle is to set new space for counter
+//    // Second clk cycle is to reset counter and bram output register
+//    // Third clk cycle is to wait
+//    // Forth clk sycle is to start data transfer
+//    serial_to_parallel
+//    #(
+//        .OUTPUT_WIDTH (   4   ),
+//        .BIT_ORDER    ( "MSB" )   // "MSB"  // "LSB"
+//    )
+//    serial_to_parallel_inst
+//    (
+//        .i_clk           ( ),
+//        .i_serial        ( ),
+//        .i_reset         ( ),
+//        .i_enable        ( ),
+//        .o_parallel_data ( )
+//    );
+    // ----------------------------------------------------------------------------------------------
+    // Alias fsm start and finished signal
+    alias i_boot_fsm_finished = o_boot_fsm_finished;
+    alias i_start_boot_fsm    = o_start_boot_fsm;
+    // MAIN FSM
     YCC59_328CQ1_main_fsm YCC59_328CQ1_main_fsm_inst
     (
         .i_clk,
         // Report signals
         .i_init              ( BRAM_intr_to_SPI.dout[5 : 5] ),
-        .i_data_spi_finished ( ),
-        .i_func_spi_finished ( ),
+        .i_boot_fsm_finished,
         // Control signals
-        .o_mem_start_addr      (     start_addr_mux    ),
-        .o_BRAM_read_write     (    BRAM_read_write    ),
-        .o_addr_counter_enable ( enable_addres_counter ),
-        .o_addr_counter_reset  (  reset_addres_counter ),
-        .o_block_ps_access     ( ),
-        .o_start_data_spi      ( ),
-        .o_start_func_spi      ( )
+        .o_start_final_addr_mux  ( start_finish_address_mux_ctrl ),
+        .o_func_spi_seq          (          func_spi_seq         ),
+        .o_block_ps_access       ( ),
+        .o_reset_address_counter (         reset_counter         ),
+        .o_start_boot_fsm,
+        .o_main_fsm_state
     );
-    
+    // BOOT FSM
+    YCC59_328CQ1_boot_fsm YCC59_328CQ1_boot_fsm_inst
+    (
+        .i_clk,
+        // Report signals
+        .i_start_boot_fsm,
+        .i_all_data_trans_finished ( spi_data_trans_finished ),
+        .i_func_trans_finished     ( spi_func_trans_finished ),
+        // Controll signals
+        .o_spi_data_init     ( start_data_trans ),
+        .o_spi_func_init     ( start_func_trans ),
+        .o_boot_fsm_finished,
+        .o_boot_fsm_state
+    );
 endmodule : YCC59_328CQ1_programm
 /*
 
---
+++ src/design/YCC59_328CQ1_prog/data_spi.sv
@@ -0,0 +1,168 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.03.2026 18:26:20
+// Design Name: 
+// Module Name: data_spi
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_spi
+#(
+    parameter int unsigned MAIN_CLK_SIGNAL = 120000000,
+    parameter int unsigned SPI_SCLK_FREQ   = 20000000
+)
+(
+    input  logic i_clk,
+    // SPI interface
+    SPI_intr.master intr_SPI_master,
+    // Other signals
+    input  logic i_start,
+           byte  i_data_from_bram,
+    output logic o_enable_address_cn,
+                 o_trans_finished,
+                 o_all_trans_finished
+);
+    
+    // -----------------------------------------------
+    // Defenition
+    wire [25 : 0] data_to_data_spi;
+    wire start_detected,
+         data_ready,
+         start_SPI,
+         stop_SPI,
+         spi_trans_detected,
+         spi_trans_finished,
+         spi_trans_active;
+    logic start_data_forming;
+    // -----------------------------------------------
+    // React only on rise edge of i_start
+    edge_sense
+    #(
+        .EDGE_TO_DETECT ( "RISING" ) // "RISING" // "FALLING" // "BOTH"
+    )
+    rise_edge_of_start_detection
+    (
+        .i_clk,
+        .i_signal (     i_start    ),
+        .o_detect ( start_detected )
+    );
+    // -----------------------------------------------
+    // Form data in start or on negedge of intr_SPI_master.CSn or spi_trans_finished
+    data_to_spi_former data_to_spi_former_isnt
+    (
+        .i_clk,
+        .i_start          (  start_data_forming ),
+        .i_data_from_bram,
+        .o_enable_cn      ( o_enable_address_cn ),
+        .o_data_ready     (      data_ready     ),
+        .o_spi_data       (   data_to_data_spi  )
+    );
+    
+    edge_sense
+    #(
+        .EDGE_TO_DETECT ( "FALLING" ) // "RISING" // "FALLING" // "BOTH"
+    )
+    fall_edge_of_CSn
+    (
+        .i_clk,
+        .i_signal (  spi_trans_active  ),
+        .o_detect ( spi_trans_detected )
+    );
+    
+    always_comb begin
+        start_data_forming <= start_detected || (start_SPI && spi_trans_detected);
+    end
+    // -----------------------------------------------
+    // SPI start 
+    RS_latch 
+    #(
+        .INITIAL_VALUE ( 1'b0 )
+    )
+    RS_latch_spi_start
+    (
+        .i_clk,
+        .i_R   (  stop_SPI  ),
+        .i_S   ( data_ready ),
+        .o_Q   (  start_SPI )
+    );
+    // -----------------------------------------------
+    // Count to last transmission
+    counter
+    #(
+        .COUNTER_WIDTH      (      3      ),
+        .START_VALUE_SOURCE ( "PARAMETER" ),  // "PARAMETER"  // "PORT"
+        .START_VALUE        (      0      ),
+        .FINAL_VALUE_SOURCE ( "PARAMETER" ),  // "PARAMETER"  // "PORT"
+        .FINAL_VALUE        (      4      )
+    )
+    counter_inst
+    (
+        .i_clk,
+        .i_reset               (         '0         ),
+        .i_enable              ( spi_trans_finished ),
+        .i_start_value         (         '0         ),
+        .i_final_value         (         '0         ),
+        .o_value               (                    ),
+        .o_final_value_reached (      stop_SPI      )
+    );
+    // -----------------------------------------------
+    // Data SPI
+    SPI_master
+    #(
+        .INPUT_WIDTH    (        26       ),
+        .MAIN_CLK_FREQ  ( MAIN_CLK_SIGNAL ),
+        .SCLK_FREQ      (  SPI_SCLK_FREQ  ),
+        .SCLK_NOT_END   (      "YES"      ),     // "YES"    // "NO"
+        .TRANSMIT_ORDER (      "MSB"      )      // "MSB"    // "LSB"
+    )
+    SPI_master_DATA
+    (
+        .i_clk,
+        // SPI master interface 
+        .intr_SPI_master,
+        // Data to transfer
+        .i_data  ( data_to_data_spi ),
+        .i_start (     start_SPI    ),
+        // Recieved data
+        .o_recieved_data   (                      ),
+        .o_data_valid      (  spi_trans_finished  ),
+        .o_transfer_active (   spi_trans_active   )
+    );
+    // -----------------------------------------------
+    // Assign block
+    assign o_trans_finished     = spi_trans_finished;
+    assign o_all_trans_finished = spi_trans_finished && ~start_SPI;
+endmodule : data_spi
+/*
+    data_spi 
+    #(
+        .MAIN_CLK_SIGNAL ( 120000000 ),
+        .SPI_SCLK_FREQ   (  20000000 )
+    )
+    data_spi_inst
+    (
+        .i_clk                ( ),
+        // SPI interface
+        .intr_SPI_master      ( ),
+        // Other signals
+        .i_start              ( ),
+        .i_data_from_bram     ( ),
+        .o_enable_address_cn  ( ),
+        .o_trans_finished     ( ),
+        .o_all_trans_finished ( )
+    );
+*/
